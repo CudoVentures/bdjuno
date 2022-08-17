734434576323116b12598535ca091873e48da82f
avalkov <avalkov@mail.bg>
2022-08-17 10:40:28 +0300

    Fix merge typo

--- modules/utils/utils.go
+++ modules/utils/utils.go
@@ -1,8 +1,5 @@
 package utils
 
-
-import "strings"
-
 import (
 	"encoding/json"
 	"strings"
@@ -16,4 +13,3 @@ func IsJSON(str string) bool {
 	var js json.RawMessage
 	return json.Unmarshal([]byte(str), &js) == nil
 }
-
