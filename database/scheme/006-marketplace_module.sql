f7cc374a7a25d9ad05e1ee1afe3e4371ffee8853
avalkov <avalkov@mail.bg>
2022-11-10 09:51:37 +0200

    Reorganize marketplace module storage to allow easier fetching of nfts

--- database/scheme/006-marketplace_module.sql
+++ database/scheme/006-marketplace_module.sql
@@ -37,28 +37,30 @@ CREATE INDEX marketplace_collection_creator_index ON marketplace_collection (cre
 CREATE TABLE marketplace_nft
 (
     transaction_hash TEXT NOT NULL REFERENCES transaction (hash),
-    id BIGINT NOT NULL,
+    id BIGINT,
+    uid TEXT,
     token_id BIGINT NOT NULL,
     denom_id TEXT NOT NULL REFERENCES nft_denom (id),
-    price TEXT NOT NULL,
+    price DECIMAL,
     creator TEXT NOT NULL,
-    PRIMARY KEY(id),
+    PRIMARY KEY(token_id, denom_id),
     FOREIGN KEY (token_id, denom_id) REFERENCES nft_nft(id, denom_id)
 );
 
-CREATE INDEX marketplace_nft_token_id_denom_id_index ON marketplace_nft (token_id, denom_id);
+CREATE INDEX marketplace_nft_id_index ON marketplace_nft (id);
 CREATE INDEX marketplace_nft_creator_index ON marketplace_nft (creator);
+CREATE INDEX marketplace_nft_uid_index ON marketplace_nft (uid);
 
 CREATE TABLE marketplace_nft_buy_history
 (
     transaction_hash TEXT NOT NULL REFERENCES transaction (hash),
     token_id BIGINT NOT NULL,
     denom_id TEXT NOT NULL REFERENCES nft_denom (id),
-    price TEXT NOT NULL,
+    price DECIMAL NOT NULL,
     seller TEXT NOT NULL,
     buyer TEXT NOT NULL,
-    usd_price TEXT NOT NULL,
-    btc_price TEXT NOT NULL,
+    usd_price DECIMAL NOT NULL,
+    btc_price DECIMAL NOT NULL,
     timestamp BIGINT NOT NULL,
     FOREIGN KEY (token_id, denom_id) REFERENCES nft_nft(id, denom_id)
 );
