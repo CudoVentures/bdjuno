296660acb6f86d65b13709b79c58fe758467f14e
Tugay Emin <tgntrr@gmail.com>
2022-12-02 14:57:11 +0200

    quickfix cw20token migration

--- database/scheme/009-cw20token_update.sql
+++ database/scheme/009-cw20token_update.sql
@@ -1,5 +1,5 @@
-ALTER TABLE cw20token_info ADD type    TEXT NULL;
-ALTER TABLE cw20token_info ADD creator TEXT NOT NULL;
+ALTER TABLE cw20token_info ADD COLUMN IF NOT EXISTS type    TEXT NULL;
+ALTER TABLE cw20token_info ADD COLUMN IF NOT EXISTS creator TEXT NOT NULL;
 
 ALTER TABLE cw20token_balance 
 ALTER COLUMN balance TYPE TEXT;
