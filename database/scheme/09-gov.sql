5c12fbbad1acf5893a4cf89f3964617000b97d86
Kamen Stoykov <k.stoykov@razorlabs.com>
2023-09-01 08:17:27 +0000

    update gov params

--- database/scheme/09-gov.sql
+++ database/scheme/09-gov.sql
@@ -1,10 +1,8 @@
 CREATE TABLE gov_params
 (
-    one_row_id     BOOLEAN NOT NULL DEFAULT TRUE PRIMARY KEY,
-    deposit_params JSONB   NOT NULL,
-    voting_params  JSONB   NOT NULL,
-    tally_params   JSONB   NOT NULL,
-    height         BIGINT  NOT NULL,
+    one_row_id BOOLEAN NOT NULL DEFAULT TRUE PRIMARY KEY,
+    params     JSONB   NOT NULL,
+    height     BIGINT  NOT NULL,
     CHECK (one_row_id)
 );
 
