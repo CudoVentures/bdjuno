e73521efab72e90eebff4cbb5038461a3b9a12c1
SpaghettiOverload <imyours@mail.bg>
2023-10-03 15:43:06 +0300

    Update: run docker test script

--- integration_tests/run_tests_in_docker.sh
+++ integration_tests/run_tests_in_docker.sh
@@ -2,5 +2,13 @@
 SETUP_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd "$SETUP_DIR"
-docker-compose -p integration_e2e  up --build -d 
+docker-compose -p integration_e2e down -v
+docker-compose -p integration_e2e up --build -d --force-recreate
 docker-compose -p integration_e2e logs --follow tests
+
+while [ "$(docker ps -q -f name=tests)" ]; do
+    sleep 5
+done
+
+echo "Tests finished"
+docker-compose -p integration_e2e down -v
