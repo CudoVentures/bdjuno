6d19b8734916b48b9c809e6e37d7f8a0318ccfa2
Kamen Stoykov <k.stoykov@razorlabs.com>
2023-10-10 08:50:53 +0000

    make integration tests using local copy of bdjuno

--- integration_tests/set_up/start_bdjuno.sh
+++ integration_tests/set_up/start_bdjuno.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-TEST_BRANCH="automated-end-to-end-testing-v0.47.3"
+# TEST_BRANCH="cudos-dev-cosmos-v0.47.3"
 export BDJUNO_INSTALL_PATH="/tmp/cudos-test-bdjuno"
 export BDJUNO_CONFIG_PATH="/sample_configs/integration-tests-config/bdjuno"
 TESTS_PATH="$BDJUNO_INSTALL_PATH/integration_tests/tests"
@@ -8,10 +8,10 @@ HOME="$BDJUNO_INSTALL_PATH$BDJUNO_CONFIG_PATH"
 
 # cleanup from previous runs
 pkill bdjuno || true
-rm -rf $BDJUNO_INSTALL_PATH
+# rm -rf $BDJUNO_INSTALL_PATH
 
 # clone bdjuno repo and install binary
-git clone -b $TEST_BRANCH https://github.com/CudoVentures/cudos-bdjuno.git $BDJUNO_INSTALL_PATH
+# git clone -b $TEST_BRANCH https://github.com/CudoVentures/cudos-bdjuno.git $BDJUNO_INSTALL_PATH
 cd $BDJUNO_INSTALL_PATH
 make install
 
